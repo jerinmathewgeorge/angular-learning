59be7d1a3df06c14abd5461a91ffece911cd1df1
Shijo rabi suseela <shijo.rabi@shellsquare.com>
2022-08-04 13:22:02 +0530

    data

--- src/app/device-unavailable/device-unavailable.component.ts
+++ src/app/device-unavailable/device-unavailable.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class DeviceUnavailableComponent implements OnInit {
 
   constructor() { }
+  private deviceData = [{
+    
+  }]
 
   ngOnInit(): void {
   }
