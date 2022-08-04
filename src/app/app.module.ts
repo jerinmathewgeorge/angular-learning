7f174baa36d01026e9466f6b764a082c2f002e32
Sherin AS <sherin.vaheed@shellsquare.com>
2022-08-04 13:59:44 +0530

    table code push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { EventsComponent } from './events/events.component';
 import { DeviceUnavailableComponent } from './device-unavailable/device-unavailable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    DeviceUnavailableComponent
+    DeviceUnavailableComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
