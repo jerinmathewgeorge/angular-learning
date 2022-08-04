69a12e58dc81ae57093dfc891baed36677ec083d
jithujohnsonmathew7 <74446770+jithujohnsonmathew7@users.noreply.github.com>
2022-08-04 15:59:22 +0530

    Services implementation

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LayoutService } from '../services/layout.service';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  @Input() latencyList = [];
+  @Input() deviceList = [];
+  @Input() eventList = [];
+  constructor(private layoutService: LayoutService) { }
 
   ngOnInit(): void {
+    this.getDeviceList();
+    this.getEventList();
+    this.getLatencyList();
+  }
+  getDeviceList(){
+    this.layoutService.getDevicesList().subscribe((res:any)=>{
+      this.deviceList = res;
+      console.log(res);
+    })
+  }
+  getEventList(){
+    this.layoutService.getEventsList().subscribe((res:any)=>{
+      this.eventList = res;
+      console.log(res);
+    })
+  }
+  getLatencyList(){
+    this.layoutService.getLatencyList().subscribe((res:any)=>{
+      this.latencyList = res;
+      console.log(res);
+    })
   }
 
 }
--- src/app/services/layout.service.ts
+++ src/app/services/layout.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import {Device} from '.././device-unavailable/device-unavailable'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LayoutService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getDevicesList(){
+    return this.http.get('http://localhost:3000/devices/');
+  }
+  getLatencyList(){
+    return this.http.get('http://localhost:3000/latency/');
+  }
+  getEventsList(){
+    return this.http.get('http://localhost:3000/events/');
+  }
+
+  addDevices(dev:Device){
+    return this.http.post('http://localhost:3000/devices/', dev);
+  }
 }
