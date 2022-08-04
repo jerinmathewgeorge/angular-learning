86e129f7cb7a8298be81eb05cff4df9873e284a5
Shijo rabi suseela <shijo.rabi@shellsquare.com>
2022-08-04 13:03:09 +0530

    table code push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { DeviceUnavailableComponent } from './device-unavailable/device-unavailable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    DeviceUnavailableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/device-unavailable/device-unavailable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-unavailable',
+  templateUrl: './device-unavailable.component.html',
+  styleUrls: ['./device-unavailable.component.css']
+})
+export class DeviceUnavailableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
