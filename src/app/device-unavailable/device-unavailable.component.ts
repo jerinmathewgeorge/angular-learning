b5b39791c9aaf3b4cf5b8ff5a97cbef6cd0ee73b
Shijo rabi suseela <shijo.rabi@shellsquare.com>
2022-08-04 13:50:51 +0530

    data bining

--- src/app/device-unavailable/device-unavailable.component.ts
+++ src/app/device-unavailable/device-unavailable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Device } from './device-unavailable';
 
 @Component({
   selector: 'app-device-unavailable',
@@ -8,8 +9,76 @@ import { Component, OnInit } from '@angular/core';
 export class DeviceUnavailableComponent implements OnInit {
 
   constructor() { }
-  private deviceData = [{
-    
+   deviceData: Device[]=[{
+    "_id": {
+      "$oid": "62eb6fd71c47a73d87125eb8"
+    },
+    "Device": "Vienna-srv",
+    "PingState": "down",
+    "IP": "192.168.1.0",
+    "Time": "48minutes, 59seconds, 10milliseconds",
+    "Location": "Vienna",
+    "Contact": "vienna-support@gmail.com"
+  },{
+    "_id": {
+      "$oid": "62eb6fdf1c47a73d87125eb9"
+    },
+    "Device": "Vienna-srv",
+    "PingState": "down",
+    "IP": "192.168.1.0",
+    "Time": "48minutes, 59seconds, 10milliseconds",
+    "Location": "Vienna",
+    "Contact": "vienna-support@gmail.com"
+  },{
+    "_id": {
+      "$oid": "62eb6fe41c47a73d87125eba"
+    },
+    "Device": "Vienna-srv",
+    "PingState": "down",
+    "IP": "192.168.1.0",
+    "Time": "48minutes, 59seconds, 10milliseconds",
+    "Location": "Vienna",
+    "Contact": "vienna-support@gmail.com"
+  },{
+    "_id": {
+      "$oid": "62eb6fe71c47a73d87125ebb"
+    },
+    "Device": "Vienna-srv",
+    "PingState": "down",
+    "IP": "192.168.1.0",
+    "Time": "48minutes, 59seconds, 10milliseconds",
+    "Location": "Vienna",
+    "Contact": "vienna-support@gmail.com"
+  },{
+    "_id": {
+      "$oid": "62eb6fea1c47a73d87125ebc"
+    },
+    "Device": "Vienna-srv",
+    "PingState": "down",
+    "IP": "192.168.1.0",
+    "Time": "48minutes, 59seconds, 10milliseconds",
+    "Location": "Vienna",
+    "Contact": "vienna-support@gmail.com"
+  },{
+    "_id": {
+      "$oid": "62eb6fed1c47a73d87125ebd"
+    },
+    "Device": "Vienna-srv",
+    "PingState": "down",
+    "IP": "192.168.1.0",
+    "Time": "48minutes, 59seconds, 10milliseconds",
+    "Location": "Vienna",
+    "Contact": "vienna-support@gmail.com"
+  },{
+    "_id": {
+      "$oid": "62eb6fff1c47a73d87125ec5"
+    },
+    "Device": "Vienna-srv",
+    "PingState": "down",
+    "IP": "192.168.1.0",
+    "Time": "48minutes, 59seconds, 10milliseconds",
+    "Location": "Vienna",
+    "Contact": "vienna-support@gmail.com"
   }]
 
   ngOnInit(): void {
